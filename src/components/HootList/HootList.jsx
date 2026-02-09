e2a3488ddce7a280643c611da38ecb837768af27
Spencer <sweinstein34@gmail.com>
2026-02-09 13:17:53 -0500

    Updated fetchHoots useEffect to update when user changes + edited show hoot service to use axios

--- src/components/HootList/HootList.jsx
+++ src/components/HootList/HootList.jsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from "react";
 import { getHoots } from "../../services/hootService.js";
 import { Link } from "react-router";
 import { useContext } from "react";
-import { UserContext } from "./contexts/UserContext.jsx";
+import { UserContext } from "../../contexts/UserContext.jsx";
 //hoots: title, author, comments, text, category
 const HootList = () => {
   const [hoots, setHoots] = useState([]);
   const { user } = useContext(UserContext);
 
   useEffect(() => {
-    const index = async () => {
+    const fetchHoots = async () => {
       const data = await getHoots();
       console.log(data);
       setHoots(data);
